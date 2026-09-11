4f0c21f6c42eeb2c5a44f3aef29e5d33a45a6a20
mukti9537-oss <mukti9537@gmail.com>
2026-09-11 21:53:06 +0600

    Navbar style change

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -7,16 +7,23 @@ const Navbar = () => {
                 <img src={Logo} className="w-[100] h-9" alt="" />
 
                 <ul className="flex gap-4 items-center text-[#475569]">
-                    <li><a href="/" className="text-[#DB2777]">Home</a></li>
-                    <li><a href="/">Technologics</a></li>
-                    <li><a href="/">Projects</a></li>
-                    <li><a href="/">About</a></li>
-                    <li><a href="/">Contact</a></li>
+
+                    <li className="active:scale-90 transition-transform"><a href="/" className="text-[#DB2777]">Home</a></li>
+
+                    <li className="active:scale-90 transition-transform"><a href="/" >Technologics</a></li>
+
+                    <li className="active:scale-90 transition-transform"><a href="/">Projects</a></li>
+
+                    <li className="active:scale-90 transition-transform"><a href="/">About</a></li>
+
+                    <li className="active:scale-90 transition-transform"><a href="/" >Contact</a></li>
                 </ul>
 
                 <div className="flex gap-4 items-center">
-                    <button className="text-[#334155]">Sign In</button>
-                    <button className="bg-[#DB2777] rounded-3xl px-4 py-2 text-white">Sign up</button>
+                    <button className="text-[#334155] active:scale-90 transition-transform cursor-pointer">Sign In</button>
+
+                    <button className="bg-[#DB2777] rounded-3xl px-4 py-2 text-white 
+                    hover:scale-110 active:scale-90 transition-transform cursor-pointer">Sign up</button>
                 </div>
             </nav>
         </div>
