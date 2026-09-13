f63708a3f2161fff2a8efa5c90ef821757d25404
mukti9537-oss <mukti9537@gmail.com>
2026-09-13 19:42:02 +0600

    Toastify apply

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import TechnologyCards from "./component/Technology Cards/TechnologyCards"
 import type { TechnologyCardsType } from "./Types/TechnologycardsType";
 import Stack from "./component/Stack/Stack";
 import Footer from "./component/Footer";
+import { ToastContainer , toast } from "react-toastify";
+import "react-toastify/ReactToastify.css"
 
 const TechnologyCardsFetch = async (): Promise<TechnologyCardsType[]> => {
   const res = await fetch("/data.json");
@@ -20,31 +22,51 @@ function App() {
 
   // add technology to stack 
   const handleAddToStack = (technology: TechnologyCardsType) => {
-
+    
     setStack((previousStack) => {
 
       // same card not repeat
       const alreadyAdded = previousStack.some(
         (item) => item.id === technology.id
       );
-      if (alreadyAdded) {
-        return previousStack;
-      }
+
+    if(alreadyAdded){
+      toast.warning(`${technology.name} is already in your stack!`)
+      return previousStack;
+    }
+ toast.success(`${technology.name} added to stack`);
+
       return [...previousStack, technology];
     });
   };
+
   //remove one technology 
   const handleRemoveFromStack = (id: TechnologyCardsType["id"]
 
   ) => {
 
-    setStack((previousStack) =>
-      previousStack.filter((item) => item.id !== id)
-    );
+    setStack((previousStack) => {
+      const removeTechnology = previousStack.find(
+        (item) => item.id === id
+      );
+      if (removeTechnology) {
+        toast.error (`${removeTechnology.name} removed from stack!`);
+      }
+      return previousStack.filter(
+        (item) => item.id !== id
+      );
+
+    });
   };
+
   //remove all technology
   const handleRemoveAll = () =>{
+    if(stack.length === 0 ) {
+      toast.info("stack is already empty!");
+      return ;
+    }
     setStack([]);
+    toast.success("All technology removed!")
   };
 
   return (
@@ -82,6 +104,8 @@ function App() {
       </div>
 
        <Footer />
+
+       <ToastContainer />
 
 
     </>
