44e6146fbd0354c79884fc710b7a3ff95b91d90f
mukti9537-oss <mukti9537@gmail.com>
2026-09-13 22:02:14 +0600

    Ready for deploy

--- src/component/Technology Cards/Technologies.tsx
+++ src/component/Technology Cards/Technologies.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import type { TechnologyCardsType } from '../../Types/TechnologycardsType';
 
 interface TechnologiesProps {
--- src/component/Technology Cards/TechnologyCards.tsx
+++ src/component/Technology Cards/TechnologyCards.tsx
@@ -1,4 +1,4 @@
-import React, { use } from 'react';
+import { use } from 'react';
 import type { TechnologyCardsType } from '../../Types/TechnologycardsType';
 import Technologies from './Technologies';
 
